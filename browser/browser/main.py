fa0e66dc06906ed8bef52fc2b34eb53b4948de24
Stephen Mwangi <mail@stephenmwangi.com>
2024-10-25 21:37:11 +0300

    fix proxy bypass behavior

--- browser/browser/main.py
+++ browser/browser/main.py
@@ -7,26 +7,44 @@ import requests
 from connect.utils.terminal.markdown import render
 
 
-def get_proxies():
-    cmd = "snapctl get --view :proxy-observe https"
-    proc = subprocess.run(cmd.split(), capture_output=True, text=True)
+def get_proxy_config():
+    config = {}
+    for protocol in ["http", "https"]:
+        cmd = f"snapctl get --view :proxy-observe {protocol} -d"
 
-    if proc.returncode != 0:
-        raise RuntimeError(proc.stderr)
+        proc = subprocess.run(cmd.split(), capture_output=True, text=True)
 
-    proxies = json.loads(proc.stdout)
-    return {
-        "https": proxies["https"]["url"]
-    }
+        if proc.returncode != 0:
+            continue
+
+        config.update(json.loads(proc.stdout))
+
+    return config
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", type=str)
     args = parser.parse_args()
+    url: str = args.url
 
-    proxies = get_proxies()
-    response = requests.get(args.url, proxies=proxies)
+    bypass = []
+    proxy_config = get_proxy_config()
+
+    if url.startswith("https"):
+        if "https" in proxy_config:
+            bypass = proxy_config["https"]["bypass"]
+    elif url.startswith("http"):
+        if "http" in proxy_config:
+            bypass = proxy_config["http"]["bypass"]
+
+    if any(url.startswith(no_proxy_url) for no_proxy_url in bypass):
+        response = requests.get(url)
+    else:
+        response = requests.get(
+            url,
+            proxies={k: v["url"] for k, v in proxy_config.items()},
+        )
 
     text = html2text.html2text(response.text)
     print(render(text))
